0a167da2ba3d918f98d8a5b3444e8349b183f6bd
Cheleste1994 <wolverines@tut.by>
2023-06-16 12:00:39 +0300

    feat: add favicon and icons in header

--- rs-css/webpack.config.js
+++ rs-css/webpack.config.js
@@ -3,50 +3,58 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslingPlugin = require('eslint-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index'),
-    mode: 'development',
-    module: {
-        rules: [
-            {
-              test:  /\.ts$/i,
-              use: 'ts-loader',
-            },
-            {
-              test: /\.css$/i,
-              use: ['style-loader', 'css-loader'],
-            },
-            {
-              test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-              type: 'asset/resource',
-            },
-            {
-              test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-              type: 'asset/inline',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.json'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
-        new CleanWebpackPlugin(),
-        new EslingPlugin({ extensions: 'ts' })
+  entry: path.resolve(__dirname, './src/index'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'icons/[name][ext]',
+        },
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new EslingPlugin({ extensions: 'ts' }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/assets/icons'),
+          to: 'icons',
+        },
+      ],
+    }),
+  ],
 };
 
 module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
 
-    return merge(baseConfig, envConfig);
+  return merge(baseConfig, envConfig);
 };
