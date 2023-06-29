c94f51572145ebaab1dff21339a0d48e6e1c7cc7
Cheleste1994 <wolverines@tut.by>
2023-06-29 12:13:45 +0300

    fix: removed promise

--- rs-css/src/index.ts
+++ rs-css/src/index.ts
@@ -2,37 +2,33 @@ import './global.css';
 import Game from './components/control/app';
 import ListLevels from './assets/data/level.json';
 
-function loadGameSave(): Promise<void> {
-  return new Promise((resolve) => {
-    if (!localStorage.getItem('level')) {
-      localStorage.setItem('level', '0');
-    }
+function loadGameSave(): void {
+  if (!localStorage.getItem('level')) {
+    localStorage.setItem('level', '0');
+  }
 
-    if (localStorage.getItem('save')) {
-      const save = localStorage.getItem('save');
-      save?.split(',').forEach((value, index) => {
-        ListLevels[index].isWin = value === 'true';
-      });
-    }
-
-    resolve();
-  });
+  if (localStorage.getItem('save')) {
+    const save = localStorage.getItem('save');
+    save?.split(',').forEach((value, index) => {
+      ListLevels[index].isWin = value === 'true';
+    });
+  }
 }
 
-async function startGame(): Promise<void> {
-  try {
-    await loadGameSave();
-    const game = new Game();
-    game.start();
+function startGame(): void {
+  loadGameSave();
+  const game = new Game();
+  game.start();
 
-    window.addEventListener('load', (event) => {
+  window.addEventListener('load', (event) => {
+    try {
       const { body } = event.target as Document;
       body.style.opacity = '1';
-    });
-  } catch (error) {
-    localStorage.clear();
-    document.location.reload();
-  }
+    } catch (error) {
+      localStorage.clear();
+      document.location.reload();
+    }
+  });
 }
 
 startGame();
